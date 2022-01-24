8bc8e3a3d6a6d3eb676b47d9905097e4e596cd19
aaron <aron.moles@gmail.com>
2022-01-24 11:47:24 +0100

    refactor: rename log function file

--- scripts/functions/_index.sh
+++ scripts/functions/_index.sh
@@ -2,7 +2,7 @@
 
 if ! ${DOT_MAIN_SOURCED:-false}; then
   source "$DOTFILES_PATH/scripts/functions/args.sh"
-  source "$DOTFILES_PATH/scripts/functions/logs.sh"
+  source "$DOTFILES_PATH/scripts/functions/log.sh"
 
   readonly DOT_MAIN_SOURCED=true
 fi
