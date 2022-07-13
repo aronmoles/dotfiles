dbb4af00ef41afe121f112c9cb82fa3196b44d1a
Aarón Molés <aron.moles@gmail.com>
2022-07-13 08:25:44 +0200

    feat: add nvm and unibo alias

---
+++ terminal/_aliases/unibo.sh
@@ -0,0 +1,2 @@
+alias stg-jumpbox='gcloud config set project unibo-stg-gcp && gcloud compute ssh stg-jumpbox --zone=europe-west1-d'
+alias prod-jumpbox='gcloud config set project unibo-gcp && gcloud compute ssh prod-jumpbox --zone=europe-west1-d'
--- terminal/_configs/nvm.sh
+++ terminal/_configs/nvm.sh
@@ -22,3 +22,8 @@
 #}
 #add-zsh-hook chpwd load-nvmrc
 #load-nvmrc
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
