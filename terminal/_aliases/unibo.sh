f2643f2cfe3baf490bb30607d3c672b54d78860c
Aarón Molés <aron.moles@gmail.com>
2022-07-13 08:39:50 +0200

    feat: change set project in unibo alias

--- terminal/_aliases/unibo.sh
+++ terminal/_aliases/unibo.sh
@@ -1,2 +1,2 @@
-alias stg-jumpbox='gcloud config set project unibo-stg-gcp && gcloud compute ssh stg-jumpbox --zone=europe-west1-d'
-alias prod-jumpbox='gcloud config set project unibo-gcp && gcloud compute ssh prod-jumpbox --zone=europe-west1-d'
+alias stg-jumpbox='gcloud compute ssh stg-jumpbox --zone=europe-west1-d --project unibo-stg-gcp'
+alias prod-jumpbox='gcloud compute ssh prod-jumpbox --zone=europe-west1-d --project unibo-gcp'
