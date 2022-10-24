0de4d54024d8af99264d1c1fe8af9c1a98c6b8a1
Aarón Molés <aron.moles@gmail.com>
2022-10-24 09:54:10 +0200

    feat: brew dump

---
+++ terminal/_aliases/terraform.sh
@@ -0,0 +1,7 @@
+alias t='terraform'
+alias tp='terraform plan'
+alias ta='terraform apply'
+alias ta-y='terraform apply -auto-approve'
+alias td='terraform destroy'
+alias td-y='terraform destroy -auto-approve'
+
--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -6,6 +6,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 paths=(
   "$HOME/bin"
   "$DOTFILES_PATH/scripts"
+  /opt/homebrew/opt/openjdk@11/bin
   "$PATH"
 )
 
