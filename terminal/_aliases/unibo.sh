a085b69d3eaced46289e17ff290d267db3426e47
aronmoles <aron.moles@gmail.com>
2022-11-14 15:59:47 +0100

    Delete unibo.sh

--- terminal/_aliases/unibo.sh
+++
@@ -1,2 +0,0 @@
-alias stg-jumpbox='gcloud compute ssh stg-jumpbox --zone=europe-west1-d --project unibo-stg-gcp'
-alias prod-jumpbox='gcloud compute ssh prod-jumpbox --zone=europe-west1-d --project unibo-gcp'
