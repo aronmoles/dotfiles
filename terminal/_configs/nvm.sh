59671507c66985b7bb9ac6c6a758a7512fba4995
aaronmoles <amoles@latencydata.com>
2023-12-20 10:03:04 +0100

    feat: configure nvm

--- terminal/_configs/nvm.sh
+++ terminal/_configs/nvm.sh
@@ -23,6 +23,8 @@
 #add-zsh-hook chpwd load-nvmrc
 #load-nvmrc
 
+export NVM_NODEJS_ORG_MIRROR=http://nodejs.org/dist
+
 export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
